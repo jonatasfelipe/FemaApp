2c40102888a042f613da6fa6138567d6b3e41bdc
Jônatas Felipe Santos Gonçalves <33453320+jonatasfelipe@users.noreply.github.com>
2018-07-27 12:35:51 -0300

    Initial commit

---
+++ app/src/main/java/fema/edu/br/femaapp/TelaInicial.java
@@ -0,0 +1,41 @@
+package fema.edu.br.femaapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import static fema.edu.br.femaapp.R.layout.activity_tela_inicial;
+
+public class TelaInicial extends AppCompatActivity {
+
+    ImageButton btnAluno, btnProfessor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_tela_inicial);
+
+        btnAluno = findViewById(R.id.btnAluno);
+        btnProfessor = findViewById(R.id.btnProfessor);
+
+        btnAluno.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent abreLogin = new Intent(TelaInicial.this, TelaLoginAluno.class);
+                startActivity(abreLogin);
+            }
+        });
+
+        btnProfessor.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent abreLogin2 = new Intent(TelaInicial.this, TelaLoginProfessor.class);
+                startActivity(abreLogin2);
+            }
+        });
+
+    }
+
+
+}
---
+++ app/src/main/java/fema/edu/br/femaapp/TelaMenualuno.java
@@ -0,0 +1,13 @@
+package fema.edu.br.femaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaMenualuno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_menu_aluno);
+    }
+}
